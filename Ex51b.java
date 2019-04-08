a57935d96ce5b2785bdaf867ba3ec563cc4ddc57
Onurcan Köken <onur-koken@hotmail.com>
2019-04-08 19:33:02 +0900

    fifth week

---
+++ Ex51b.java
@@ -0,0 +1,11 @@
+public class Ex51.a{
+	public static void main (String args[]){
+		int n = 10;
+		do{
+			if (n<=0) break;
+			System.out.println(n);
+			n = n-3;
+		}
+		while(n>0); // end do while		
+	}
+}
